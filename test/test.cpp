c15ab919918601aef315f1ad9c364274d7a24082
Michael Buerger <michaelbuergerart@gmail.com>
2021-12-15 14:06:47 -0700

    Proper indices handling for OBJ files

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <memory>
-#include <array>
+#include <vector>
 
 #include "CPPML/logging/logging.hpp"
 #include "CPPML/loading/loading.hpp"
@@ -12,29 +12,38 @@
 using namespace CPPML;
 
 int main() {
-    CPPML::Log::Init(true); // true turns debug log on, false turns debug log off
-    // CPPML::Log::Init(spdlog::level::warn); // alternatively, the logger can be set to only display a certain level or above
+   CPPML::Log::Init(true); // true turns debug log on, false turns debug log off
+   // CPPML::Log::Init(spdlog::level::warn); // alternatively, the logger can be set to only display a certain level or above
 
-    FILE* model = OpenFile("test/models/triangle.obj");
+   FILE* modelFile = OpenFile("test/models/cube.obj");
 
-    std::unique_ptr<float[]> vertices;
-    std::unique_ptr<float[]> texcoords;
-    std::unique_ptr<float[]> normals;
-    uint vertexCount;
+   std::vector<float> positions;
+   std::vector<float> textureCoords;
+   std::vector<float> normals;
+   std::vector<uint> indices;
 
-    bool success = LoadOBJFile(model, vertices, texcoords, normals, vertexCount);
+   bool success = LoadOBJFile(modelFile, positions, textureCoords, normals, indices);
 
-    for(size_t i=0;i<vertexCount * 3;i++) {
-       CPPML_INFO("v {}", vertices[i]);
-    }
+   if(!success)
+   {
+      CPPML_ERROR("Loading OBJ File Unsuccessful");
+   }
 
-    for(size_t i=0;i<vertexCount * 2;i++) {
-       CPPML_INFO("vt {}", texcoords[i]);
-    }
+   for(uint i=0; i < positions.size(); i++) {
+      CPPML_INFO("v {}", positions[i]);
+   }
 
-    for(size_t i=0;i<vertexCount * 3;i++) {
-       CPPML_INFO("vn {}", normals[i]);
-    }
+   for(uint i=0; i < textureCoords.size(); i++) {
+      CPPML_INFO("vt {}", textureCoords[i]);
+   }
 
-    return 0;
+   for(uint i=0;i < normals.size(); i++) {
+      CPPML_INFO("vn {}", normals[i]);
+   }
+
+   for(uint i=0; i < indices.size(); i++) {
+      CPPML_INFO("i {}", indices[i]);
+   }
+
+   return 0;
 }
